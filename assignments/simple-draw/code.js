f4e17bcbde91611bbb1380a51cfc6962869417c8
eurosteppercy <111394603+eurosteppercy@users.noreply.github.com>
2022-08-18 15:11:16 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -36,8 +36,8 @@
  */
 
 drawLine(0, 0, width/150, height, 'red')
-drawLine(0, 0, width, height, 'red')
-drawLine(-100, 0, width, height/150, 'red')
+drawLine(0, 0, width, height, 'pink')
+drawLine(-100, 0, width, height/150, 'green')
 
 //drawCircle(width/2, height/2, 100, 'blue');
 
