c65ef9f79f383f7c7097c761a979eb3eb20ac1ae
eurosteppercy <111394603+eurosteppercy@users.noreply.github.com>
2022-08-18 15:00:42 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -43,4 +43,4 @@ drawRect(50, 50, 60, 40, 'black');
 
 drawFilledCircle(width - 300, height - 300, 50, 'purple');
 
-drawFilledRect(5, height - 20, 45, 15, 'green');
+drawFilledRect(5, height - 20, 45, 150, 'green');
